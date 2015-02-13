a4e97b32a6c87f73ba04cd182765bc6206a4d880
Zander Allen <ztrain01@gmail.com>
2015-02-12 18:52:38 -0700

    first commit

---
+++ controller.js
@@ -0,0 +1,12 @@
+var app = angular.module('userProfiles');
+
+app.controller('MainController', function($scope, mainService) {
+
+  $scope.getUsers = function() {
+  	$scope.users = mainService.getUsers();
+  }
+
+
+  $scope.getUsers();
+
+});
